ab335a62d55dbf463516458822a44e5eeb118c45
Inna Klymovych <98186099+InnaKlymovych@users.noreply.github.com>
2023-02-16 20:57:48 +0000

    Adding Javascript for sound bar

---
+++ script.js
@@ -0,0 +1,18 @@
+
+window.onload=()=>{
+    
+    const volumeSlider = document.querySelector("input");
+
+
+    const changeSliderValue = () => {
+
+        volumeSlider.style.width = `${volumeSlider.value}%`;
+
+        document.documentElement.style.setProperty(
+        "--slider-position",
+        `${volumeSlider.value}%`
+        );
+    };
+
+volumeSlider.addEventListener("input", changeSliderValue);
+};
